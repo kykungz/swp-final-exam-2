88d3f7c75e2f68e96a0b8e14eb570b3bc999e6b4
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2019-05-23 15:55:58 +0700

    asdf

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'BuSbJ3Nkq1uI3cnQ6nDI2co9o',
+  consumer_secret: 'SdIhS5eP2RXE7qneujQZgNhzNwRXBCbrDFkWyqaCLBfRN5WNuG',
+  access_token_key: '1129415270097752064-5FTRp0y3zqkmPeaMMh4h49tTlQXeFt',
+  access_token_secret: 'o0ByISoBY1UwhW233ugANTs5VbihMnhNAM09k7e8OoIoF',
+}
